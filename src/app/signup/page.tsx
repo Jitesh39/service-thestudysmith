4b2773ffbc7e6e1195ac5a001034a71b99cc9283
Jitesh Kumar <jiteshdav3931@gmail.com>
2026-02-10 00:18:04 +0530

    Spam email added in notification

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -93,7 +93,7 @@ export default function SignupPage() {
             await signOut(auth);
 
             // Redirect to login with a message
-            alert("Account created successfully! Please check your email to verify your account before logging in.");
+            alert("Account created successfully! Please check your email ( Spam Folder ) to verify your account before logging in.");
             router.push("/login");
 
         } catch (err: any) {
